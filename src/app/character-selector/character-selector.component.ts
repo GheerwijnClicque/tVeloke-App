34f759fbfb84994740caca8e023d0a2556b4327e
GheerwijnClicque <gheerwijn.clicque@clarabridge.com>
2018-04-16 20:32:53 +0200

    Mss ook die component toevoegen eh

---
+++ src/app/character-selector/character-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'character-selector',
+  templateUrl: './character-selector.component.html',
+  styleUrls: ['./character-selector.component.scss']
+})
+export class CharacterSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
